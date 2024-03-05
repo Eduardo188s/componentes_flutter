a411e6b2633bb1a5fcd043d9746f1dff306497a0
Eduardo188s <eduardodelarosa188@gmail.com>
2024-03-05 12:02:00 -0600

    Images

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica_3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -14,12 +16,60 @@ class _ImagesScreenState extends State<ImagesScreen> {
       appBar: AppBar(
         title: const Text('Imagenes'),
       ),
-      body:const  Column(
+      body: ListView(
         children: [
-           Image(
-            image: AssetImage('assets/img/InverNova.jpg')),
+           cardImage1(),
+           image2(),
+           imageFade(),
         ],
       ),
+    );
+  }
+  Card cardImage1(){
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      margin: const EdgeInsets.all(15),
+      elevation: 15,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Column(
+        children: [
+          const Image(
+            image: AssetImage('assets/img/InverNova.jpg')
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'InverNova',
+                style: AppTheme.ligthTheme.textTheme.headlineLarge,
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+  SizedBox image2(){
+    return SizedBox(
+      height: 200,
+      width: 100,
+      child: Image.network(
+       'https://th.bing.com/th/id/OIP.4SFSNkGT--C7_WgDSgPgdgHaD0?rs=1&pid=ImgDetMain'
+        ),
+    );
+  }
+  Stack imageFade(){
+    return Stack(
+      children: <Widget>[
+        const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://th.bing.com/th/id/OIP.4SFSNkGT--C7_WgDSgPgdgHaD0?rs=1&pid=ImgDetMain'),
+        ),
+      ],
     );
   }
 }
